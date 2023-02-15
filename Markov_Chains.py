02f4d680b64f71306dd863e8c67c76a8fde24697
KrolSilmon <60669254+KrolSilmon@users.noreply.github.com>
2023-02-15 16:01:01 +0000

    changed the date range mechanism

--- Markov_Chains.py
+++ Markov_Chains.py
@@ -7,16 +7,23 @@ from random import randint
 
 dataset_address = 'MCD.csv'
 cut_off_date = '2010-01-01'
-number_of_days = 100
+
 data = pd.read_csv(dataset_address)
 print(data.head())
+end_date = max(data['Date'])
+
+date_format = "%Y-%m-%d"
+cutoff = datetime.datetime.strptime(cut_off_date, date_format)
+last_date = datetime.datetime.strptime(end_date, date_format)
+
 data['Date'] = pd.to_datetime(data['Date'])
 rows_per_sequence = 10
 data_in_date = data[data['Date'] >= cut_off_date]
+number_of_days = last_date - cutoff 
+number_of_days = number_of_days.days
+print(data_in_date)
 
-print(data_in_date.head())
-
-new_row_set = []
+difference_row_set = []
 start_row = 0
 counter = 0
 for row_set in range(0,number_of_days):
@@ -25,7 +32,7 @@ for row_set in range(0,number_of_days):
     
     end_row = start_row + rows_per_sequence
     market_subset = data_in_date.iloc[start_row : end_row]
-    close_date = max(market_subset['Date'])
+    close_date = max(market_subset['Date'], default = 0)
     
     close_gap = market_subset['Close'].pct_change()
     high_gap = market_subset['High'].pct_change()
@@ -34,7 +41,7 @@ for row_set in range(0,number_of_days):
     daily_change = (market_subset['Close'] - market_subset['Open']) / market_subset['Open']
     close_price_next_day_direction = (market_subset['Close'].shift(-1) - market_subset['Close'])
     
-    new_row_set.append(pd.DataFrame({
+    difference_row_set.append(pd.DataFrame({
         'Seuquence ID' : [row_set] * len(market_subset),
         'End Date' : [close_date] * len(market_subset),
         'Close Gap' : close_gap,
@@ -47,6 +54,8 @@ for row_set in range(0,number_of_days):
     
     counter += 1
 
-new_row_set = pd.concat(new_row_set)
-new_row_set = new_row_set.dropna(how='any')
-new_row_set.to_csv('NRS.csv', index=False)
+difference_row_set = pd.concat(difference_row_set)
+difference_row_set = difference_row_set.dropna(how='any')
+difference_row_set.to_csv('NRS.csv', index=False)
+
+
