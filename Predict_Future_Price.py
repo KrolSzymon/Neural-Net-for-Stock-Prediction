8ed7b12b0c718af45d02a0adb04340b524c2e5c9
Simon Kotyza <szajmecki@gmail.com>
2023-03-23 17:18:12 +0000

    Cleaned up

--- Predict_Future_Price.py
+++ Predict_Future_Price.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as pp
 
 
 timesteps_in_future = 10
-model = load_model('model_btc.h5 ')
+model = load_model('model_btc.h5')
 dataset = 'MCD.csv'
 scaler = MinMaxScaler()
 lookback = 30
@@ -28,7 +28,7 @@ def predict_future(transformed_dataset, training_range):
     prediction_list = prediction[-training_range:]    
     for _ in range(timesteps_in_future):
         transformed_dataset = prediction_list[-training_range:]
-        transformed_dataset = transform_dataset.reshape((1, training_range, 1))
+        transformed_dataset = transformed_dataset.reshape((1, training_range, 1))
         out = model.predict(transformed_dataset)[0][0]
         prediction_list = np.append(prediction_list, out)
     prediction_list = prediction_list[training_range-1:]
